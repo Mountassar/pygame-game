978375096dae5d0ef1e06e91bc1e238d7c908c36
Mountassar <mountassaralimi@gmail.com>
2021-08-09 09:11:21 -0400

    listen to specific user keyboard input in main loop

--- main.py
+++ main.py
@@ -9,40 +9,37 @@ from pygame.locals import (
     KEYDOWN,
     QUIT,
 )
-
 pygame.init()
+pygame.get_init()
 # create screen
 screen = pygame.display.set_mode((1000,1000))
-# player
-playerX = 500
-playerY = 800
+#name caption
 pygame.display.set_caption('Plane Simulator')
-playerImg = pygame.image.load('images/pp.png')
-#player display function
+screen.fill((255,200,200))
+# player definition
+playerX = 300
+playerY = 300
+plane = pygame.image.load('images/pp.png')
 #create second surface
-surface_2 = pygame.Surface([playerX,playerY])
-def player(playerX , playerY):
-
-    scaled = pygame.transform.scale(playerImg, (500, 400))
-    screen.blit (scaled, (playerX, playerY))
-clock = pygame.time.Clock()
- # main loop
+surface_2 = pygame.Surface((playerX, playerY))
+# main loop
 running = True
 while running:
-    player(playerX , playerY)
-    screen.blit(surface_2, (playerX,playerY))
-
+    screen.fill((255,255,255))
+    screen.blit(plane,(playerX,playerY))
     for event in pygame.event.get():
-        print(id(event.type))
         if event.type == QUIT:
             running = False
-
         if event.type == KEYDOWN:
+            if event.key == K_LEFT:
+                playerX -= 10
+            if event.key == K_RIGHT:
+                playerX += 10
             if event.key == K_UP:
                 playerY -= 10
-                player(playerX, playerY)
-
-
+            if event.key == K_DOWN:
+                playerY += 10
+    pygame.display.flip()
 
 
 
